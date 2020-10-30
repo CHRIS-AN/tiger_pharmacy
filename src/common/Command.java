23bc0e55f7549c6ed1886c052d2b396c89239a36
dora4 <dora4@DESKTOP-2TOI1RP>
2020-10-30 14:04:47 +0900

    Command 이름 수정

--- src/common/Command.java
+++ src/common/Command.java
@@ -3,6 +3,6 @@ package common;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface command {
+public interface Command {
 	void execute(HttpServletRequest request, HttpServletResponse response);
 }
