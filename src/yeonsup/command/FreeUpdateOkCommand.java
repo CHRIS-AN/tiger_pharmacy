5a483d39548469db1d3ad7b0b5cead59f6fe03ab
dora4 <dora4@DESKTOP-2TOI1RP>
2020-11-09 11:34:28 +0900

    FreeUpdateOkCommmand

--- src/yeonsup/command/FreeUpdateOkCommand.java
+++ src/yeonsup/command/FreeUpdateOkCommand.java
@@ -64,7 +64,7 @@ public class FreeUpdateOkCommand implements Command {
 		int b_uid = Integer.parseInt(multi.getParameter("b_uid"));
 		String delFile = multi.getParameter("delfile");	
 		
-		if(delFile.length() > 0 && delFile != null) { // 삭제할 대상 파일이 있다면
+		if(delFile != null) { // 삭제할 대상 파일이 있다면
 			
 			dao.deleteByUid(b_uid, request);
 	
